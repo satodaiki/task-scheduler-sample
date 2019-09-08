14fed115b715031d9eb48c575c8a001b042ced93
satodai <banana5388@gmail.com>
2019-09-08 22:55:59 +0900

    first commit.

---
+++ task-scheduler-sample/UIImage.swift
@@ -0,0 +1,11 @@
+//
+//  UIImage.swift
+//  task-scheduler-sample
+//
+//  Created by 佐藤大樹 on 2019/09/07.
+//  Copyright © 2019 佐藤大樹. All rights reserved.
+//
+
+import Foundation
+
+let image = UIImage(named: "tit_ball.jpg")
--- task-scheduler-sample/ViewController.swift
+++ task-scheduler-sample/ViewController.swift
@@ -10,11 +10,24 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var titBallImage: UIImageView!
+    @IBOutlet weak var touchCountStr: UILabel!
+    
+    var touchCount: Int = 1
+    
+    @IBAction func titTapAction(_ sender: Any) {
+        touchCountStr.text = String(touchCount)
+        touchCount = touchCount + 1
+    }
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        
+        // バンドルした画像ファイルを読み込み
+        let image = UIImage(named: "tit_ball")
+        
+        // Image Viewに画像を設定
+        titBallImage.image = image
     }
-
-
 }
 
